c572d8d69911cc07cda827df2adb1684f25bf397
Kwanjirach <kwanjirachandaeng67@gmail.com>
2022-09-25 22:33:24 +0700

    Hw1

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,32 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
+  if(arrayOfString == null || undefined){
+    return undefined
+}
+else{
+    let max_str = arrayOfString[0].length
+    let ans_max = arrayOfString[0]
+
+    for(let i = 1; i < arrayOfString.length;i++){
+        let max_str_i = arrayOfString[i].length;
+        if(max_str_i > max_str){
+            ans_max = arrayOfString[i];
+            max_str = max_str_i;
+        }
+        else if(max_str_i == max_str && ans_max != arrayOfString[i]){
+            ans_max = [`${ans_max}`,`${arrayOfString[i]}`]
+            return ans_max
+        }
+    }
+    return ans_max = [ans_max]
+}
 }
 module.exports = getMaxLengthString
+console.log(getMaxLengthString(['hello', 'hi', 'hey']))
+console.log(getMaxLengthString(['hello', 'hi', 'hey', 'Good Evening', 'Good Morning']))
+console.log(getMaxLengthString(['hi']))
+console.log(getMaxLengthString(null))
+console.log(getMaxLengthString(undefined))
+//  const arrayOfString = ['hello', 'hi', 'hey']
+//  console.log(arrayOfString[0].length)
